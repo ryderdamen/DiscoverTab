eeb95de5c988c6331dc2c341d668f700f69a8a7e
Ryder <dev@ryderdamen.com>
2017-10-28 17:21:58 -0400

    Main page refactor

--- js/main.js
+++ js/main.js
@@ -1,119 +1,129 @@
 /*
 Main.js
+Version: 1.0
+Author: Ryder Damen
+ryderdamen.com/discovertab
 
-1. Retrieve geolocation
-2. On callback with lat and lon, store in JSON, load map
-3. Fade map in to background
+Page Overview:
+1. Retrieves user preferences from storage (as set by the options page)
+2. Sets variable CSS classes based on those stored preferences
+3. Sets variable HTML objects based on stored preferences
+4. Retrieves the last known position of the user, and uses it to implement a google map; fades the map when implemented
+5. Retrieves the currrent known position of the user, and stores if for next time.
 
-Notes: To prevent delays, the map is always 1 load behind the user // TODO: fix this? 
-can't hide map, too many issues with fade in;
+// NB: We are retriving the last known position of the user, because the callback for the current location is far too long (3 seconds)
+
+// TODO: Implement GMAPS API key; implement random mode
 
 */
 
-// Temp hardcoded variables
-let pageStylePreference = "light"; // Hardcoded for now
-let userName = "Ryder Damen";
+
+// Page Initialization
+initializePage();
 
 
+// Functions -------------------------------------------------------------------------------------------------------------
 
-
-
-// Then, when the page loads...
-window.onload = loadPage();
-
-// Then, when the page is finished loading...
-$( document ).ready(function() {
-	// On document ready, retrieve last location variables and load the map
-	chrome.storage.sync.get(['lastLat','lastLon'], function (result) {
-		
-		if (!result.lastLat) {
-			// Display Toronto 43.6425662,-79.3892508
-			initMap(43.6425662, -79.3892508);
+function initializePage() {
+	// Initializes the page by retrieving preferences from storage
+	chrome.storage.sync.get(
+	[	'discoverTabTheme',
+		'discoverTabUserName',
+		'discoverTabZoom',
+		'discoverTabGmapsApiKey',
+		'discoverTabRandomMode'
+	], function (stored) {
+		// Now that we have the variables, continue with loading:
+		if (stored.discoverTabUserName === undefined) {
+			// This is our first time loading
+			constructPage("", 0, 15, "", false);
 		}
 		else {
-			initMap(result.lastLat, result.lastLon);
+			constructPage(stored.discoverTabUserName, stored.discoverTabTheme, stored.discoverTabZoom, stored.discoverTabGmapsApiKey, stored.discoverTabRandomMode);
 		}
-    });
-});
+		
+		
+	});
+}
 
-
-function loadPage() {
-	// This method initializes and displays the date, and retrieves the user's location for the next load
+function constructPage(userName, theme, zoom, apiKey, randomMode) {
+	// Constructs the actual page, and applies variable classes
 	
-	//pageStylePreference = chrome.storage.sync.get(['pageStyle'], function (result) { return result; });
-	// TODO: Fix this, retrieve a global variable from JSON or write them
-	
-	// Handling Dark Styling
-	if (pageStylePreference == "dark") {
+	if (theme == 1) { // Dark Theme Class Applications
 		document.getElementById('html').className = 'dark';
 		document.getElementById('overlay').className = 'dark-overlay';
 		document.getElementById('map-cover').className = 'dark-map-cover';
 	}
 	
-	// Setting User Name
+	// Set Username
 	document.getElementById('name').innerHTML = userName;
 	
-	// Setting Date
+	// Retrieve and Set Date
 	var d = new Date();
 	var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	var months = ["January","February","March","April","May","June","July","August","September","October","November","December" ];
 	document.getElementById("date").innerHTML = days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+	
+	// Retrieve the user's location
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(positionCallback);
+		navigator.geolocation.getCurrentPosition(userLocated);
 	}
 	else {
 		console.log('Sorry, Geolocation is not supported for your browser or operating system :(');
 	}
-}
-
-
-function initMap(lat, lon) {
-	// This method initalizes the map, simple user boilerplate
-	if (pageStylePreference == "light") {
-		mapStyling = mapStyleLight;
-	}
-	else {
-		mapStyling = mapStyleDark;
-	}
-    var centre = {lat: lat, lng: lon};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: centre,
-      disableDefaultUI: true,
-      styles: mapStyling
+	
+	// Retrieve the last known latitude and longitude variables
+	chrome.storage.sync.get(['lastLat','lastLon'], function (result) {
+		if (randomMode) {
+			// If we're in random mode, return a random lat/lon from the places array
+			var randomPlace = places[Math.floor(Math.random() * places.length)];
+			document.getElementById('cityName').innerHTML = randomPlace.city + ", " + randomPlace.state;
+			initializeGoogleMap(Number(randomPlace.latitude), Number(randomPlace.longitude), 12, theme, apiKey);
+			return;
+		}
+		if (!result.lastLat) {
+			// If we don't have any stored lat/lons, show Toronto, because it's the developer's home and it's pretty
+			initializeGoogleMap(43.6425662, -79.3892508, 12, theme, apiKey);
+			return;
+		}
+		else {
+			initializeGoogleMap(result.lastLat, result.lastLon, zoom, theme, apiKey);
+			return;
+		}
     });
-    
-    // Add a listener to fade the cover out after the map is loaded
-    google.maps.event.addListenerOnce(map, 'idle', function(){ 
-    	$(".map-cover").fadeOut(2000);
-    	$(".dark-map-cover").fadeOut(2000);
-	});
-}
-      
-      
-function writeLatitudeLongitude(lastLat, lastLon) {
-    if (!lastLat) {
-      console.log("Error: No latitude or longitude");
-      return;
-    }        
-    chrome.storage.sync.set({'lastLat': lastLat, 'lastLon': lastLon}, function() {});
 }
 
-
-function writeUserPreferences() {
-	if (!pageStylePreference) {
-          console.log("Error: No user preferences to save");
-          return;
-        }        
-        chrome.storage.sync.set({'pageStyle': pageStylePreference}, function() {
-          console.log("New user preferences saved");
-        });
-}
-      
-     
-function positionCallback(position) {
+function userLocated(position) {
 	// This method is called back after a delay of around 3 seconds
 	if (!position.coords.latitude) { return; }
-	writeLatitudeLongitude(position.coords.latitude, position.coords.longitude);	    
+	// Writes the latitude and longitude to storage (preferences)
+	chrome.storage.sync.set({'lastLat': position.coords.latitude, 'lastLon': position.coords.longitude}, function() {});	
 }
 
+function initializeGoogleMap(lat, lon, zoomLevel, theme, apiKey) {
+	// First, get the google maps script with the user's API Key
+	$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + apiKey, function( data, textStatus, jqxhr ) {	 	
+		// Then, initialize the map
+		if (theme == 0) {
+			mapStyling = mapStyleLight;
+		}
+		else {
+			mapStyling = mapStyleDark;
+		}
+	    var centre = {lat: lat, lng: lon};
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	      zoom: Number(zoomLevel),
+	      center: centre,
+	      disableDefaultUI: true,
+	      styles: mapStyling
+	    });
+	    
+	    // Add a listener to fade the cover out after the map is loaded
+	    google.maps.event.addListenerOnce(map, 'idle', function(){ 
+	    	$(".map-cover").fadeOut(2000);
+	    	$(".dark-map-cover").fadeOut(2000);
+		});
+	});
+	
+}
+      
